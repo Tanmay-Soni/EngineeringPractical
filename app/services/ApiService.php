514714502db95988e7269d3c8f3dfcbc8c9d5fcf
tsoni2 <tsoni2@ncsu.edu>
2025-05-06 14:20:48 -0400

    Add ApiService to fetch weather from Open-Meteo API

---
+++ app/services/ApiService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+use Illuminate\Support\Facades\Http;
+
+class ApiService
+{
+    protected string $baseUrl = 'https://api.open-meteo.com/v1/forecast';
+
+    public function getWeather(float $lat, float $lon): array
+    {
+        try {
+            $response = Http::get($this->baseUrl, [
+                'latitude' => $lat,
+                'longitude' => $lon,
+                'current_weather' => true
+            ]);
+
+            if ($response->failed()) {
+                throw new Exception('API request failed');
+            }
+
+            return $response->json();
+        } catch (Exception $e) {
+            return [
+                'error' => $e->getMessage(),
+            ];
+        }
+    }
+}
